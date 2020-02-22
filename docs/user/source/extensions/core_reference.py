2ddf7dc3fe83dd03179b4f1a515cfdb0b7864822
Ben Beddows <drwhut@gmail.com>
2020-02-22 20:58:22 +0000

    Created section names!

--- docs/user/source/extensions/core_reference.py
+++ docs/user/source/extensions/core_reference.py
@@ -4,13 +4,19 @@ from docutils.parsers.rst import Directive
 class CoreReference(Directive):
 
     def run(self):
-        return [nodes.paragraph(text="Hi!")]
+        functions = nodes.section(ids=nodes.make_id("core-functions"))
+        functions += nodes.title("Core Functions", "Core Functions")
+
+        subroutines = nodes.section(ids=nodes.make_id("core-subroutines"))
+        subroutines += nodes.title("Core Subroutines", "Core Subroutines")
+
+        return [functions, subroutines]
 
 def setup(app):
     app.add_directive("core-reference", CoreReference)
 
     return {
-        "version": "0.1",
+        "version": "0.3.0",
         "parallel_read_safe": False,
         "parallel_write_safe": False
     }
