eb44038bef92045b5129fbbb47f181f88601937c
Ben Beddows <drwhut@gmail.com>
2020-02-22 22:16:28 +0000

    The directive now display the description and infinite inputs.

--- docs/user/source/extensions/core_reference.py
+++ docs/user/source/extensions/core_reference.py
@@ -10,12 +10,47 @@ class CoreReference(Directive):
         warning += nodes.paragraph(text=message)
         return warning
     
+    def parse_sockets(self, sockets, input):
+        return nodes.paragraph(text="Placeholder.")
+
+    def parse_definition(self, definition):
+        name = definition["name"]
+        description = definition["description"]
+
+        d = nodes.section(ids=nodes.make_id("definition-" + name))
+        d += nodes.title(name, name)
+
+        d += nodes.paragraph(text=description)
+
+        if definition["infiniteInputs"] == "true":
+            d += nodes.paragraph(text="This node allows for an infinite number of inputs.")
+        
+        inputs = nodes.section(ids=nodes.make_id("inputs-" + name))
+        inputs += nodes.title("Inputs", "Inputs")
+
+        inputs += self.parse_sockets(definition["inputs"], True)
+
+        d += [inputs]
+        
+        outputs = nodes.section(ids=nodes.make_id("outputs-" + name))
+        outputs += nodes.title("Outputs", "Outputs")
+
+        outputs += self.parse_sockets(definition["outputs"], False)
+
+        d += [outputs]
+
+        return d
+    
     def parse_json(self, core):
         functions = nodes.section(ids=nodes.make_id("core-functions"))
         functions += nodes.title("Core Functions", "Core Functions")
 
+        functions += [self.parse_definition(d) for d in core["functions"]]
+
         subroutines = nodes.section(ids=nodes.make_id("core-subroutines"))
         subroutines += nodes.title("Core Subroutines", "Core Subroutines")
+
+        subroutines += [self.parse_definition(d) for d in core["subroutines"]]
 
         return [functions, subroutines]
 
