d5e132a767d6b39fa01e7388c3d1ca88a23ed663
Ben Beddows <drwhut@gmail.com>
2020-02-22 23:53:44 +0000

    Display sockets in a list format.

--- docs/user/source/extensions/core_reference.py
+++ docs/user/source/extensions/core_reference.py
@@ -10,14 +10,50 @@ class CoreReference(Directive):
         warning += nodes.paragraph(text=message)
         return warning
     
-    def parse_sockets(self, sockets, input):
-        return nodes.paragraph(text="Placeholder.")
+    """
+    TODO:
+    For the sockets, I tried to make them tables. Really, I genuinely tried.
+    But MY GOD the Docutils documentation is baaaddd. Not only does it not have
+    any styling, but THERE IS LITERALLY NOTHING DOCUMENTED ABOUT TABLES.
+
+    So, if you know how the hell tables work in Docutils, please let me know!
+    Your knowledge and wisdom would be greatly appreciated!
+    """
+    
+    def parse_socket(self, socket, isInput):
+        header = "%s (%s)" % (socket["name"], socket["type"])
+
+        i = nodes.list_item()
+        i += nodes.paragraph(text=header)
+
+        body = nodes.bullet_list()
+        
+        desc = nodes.list_item()
+        desc += nodes.paragraph(text=socket["description"])
+
+        if isInput:
+            default = nodes.list_item()
+            defaultText = "Default value: " + str(socket["default"])
+            default += nodes.paragraph(text=defaultText)
+
+        if isInput:
+            body += [desc, default]
+        else:
+            body += [desc]
+
+        i += [body]
+        return i
+    
+    def parse_sockets(self, sockets, isInput):
+        l = nodes.enumerated_list()
+        l += [self.parse_socket(socket, isInput) for socket in sockets]
+        return l
 
     def parse_definition(self, definition):
         name = definition["name"]
         description = definition["description"]
 
-        d = nodes.section(ids=nodes.make_id("definition-" + name))
+        d = nodes.section(ids=[name])
         d += nodes.title(name, name)
 
         d += nodes.paragraph(text=description)
@@ -25,14 +61,14 @@ class CoreReference(Directive):
         if definition["infiniteInputs"] == "true":
             d += nodes.paragraph(text="This node allows for an infinite number of inputs.")
         
-        inputs = nodes.section(ids=nodes.make_id("inputs-" + name))
+        inputs = nodes.section(ids=[name + "-inputs"])
         inputs += nodes.title("Inputs", "Inputs")
 
         inputs += self.parse_sockets(definition["inputs"], True)
 
         d += [inputs]
         
-        outputs = nodes.section(ids=nodes.make_id("outputs-" + name))
+        outputs = nodes.section(ids=[name + "-outputs"])
         outputs += nodes.title("Outputs", "Outputs")
 
         outputs += self.parse_sockets(definition["outputs"], False)
@@ -42,12 +78,12 @@ class CoreReference(Directive):
         return d
     
     def parse_json(self, core):
-        functions = nodes.section(ids=nodes.make_id("core-functions"))
+        functions = nodes.section(ids=["functions"])
         functions += nodes.title("Core Functions", "Core Functions")
 
         functions += [self.parse_definition(d) for d in core["functions"]]
 
-        subroutines = nodes.section(ids=nodes.make_id("core-subroutines"))
+        subroutines = nodes.section(ids=["subroutines"])
         subroutines += nodes.title("Core Subroutines", "Core Subroutines")
 
         subroutines += [self.parse_definition(d) for d in core["subroutines"]]
